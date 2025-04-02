7fc8acb28c46e09a24abdf60ed08c6d12db67815
Renata <murzinarenata@gmail.com>
2025-04-01 19:42:20 -0700

    wave 4

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -24,10 +24,18 @@ class Vendor:
     def swap_items(self, other_vendor, my_item, their_item):
         
         if my_item in self.inventory and their_item in other_vendor.inventory:
-            self.inventory.remove(my_item)
-            other_vendor.inventory.remove(their_item)
-            self.inventory.append(their_item)
-            other_vendor.inventory.append(my_item)
+            self.remove(my_item)
+            other_vendor.add(my_item)
+
+            other_vendor.remove(their_item)
+            self.add(their_item)
+
             return True
         
+        return False
+    
+    def swap_first_item(self, other_vendor):
+        if self.inventory and other_vendor.inventory:
+            return self.swap_items(other_vendor, self.inventory[0], other_vendor.inventory[0])
+
         return False
