0c0c8e1ae0d5d9aaa111adbd8cf1e077d6580e14
Renata <murzinarenata@gmail.com>
2025-04-01 17:58:43 -0700

    wave 2

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -8,10 +8,15 @@ class Vendor:
         return item
 
     def remove(self, item):
-
         if item in self.inventory:
             self.inventory.remove(item)
-
             return item
 
         return False   
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        
+        return None
